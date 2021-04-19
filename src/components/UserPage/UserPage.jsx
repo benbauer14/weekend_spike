a7f59b3b2f5a36968756d9d6d54ed26094ed2629
Ben Bauer <ben.bauer1@gmail.com>
2021-04-19 16:20:00 -0500

    user page styled

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router';
+import './UserPage.css'
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+  const history = useHistory();
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
+      <div onClick={() => {history.push('/search')}}><Button color="primary">Search</Button></div>
+      <div onClick={() => {history.push('/trade')}}><Button color="primary">Trade/Sell Items</Button></div>
+
     </div>
   );
 }
