8e6a99c762196063d869d0d18d136eacf3ed2acd
Ben Bauer <ben.bauer1@gmail.com>
2021-04-19 17:05:46 -0500

    created chat page

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,6 +1,27 @@
+
+import DeleteIcon from '@material-ui/icons/Delete';
+import FaceIcon from '@material-ui/icons/Face';
+import './Chat.css'
+
 function Chat () {
     return (
         <>
+        <div className="chatMain">
+            <div className="chatMessageListItem">
+                <div className="userAvatar">
+                    <FaceIcon />
+                </div>
+                <div className="chatMessage">
+                    <p>from: username</p> 
+                    <p>date: 1/1/2021</p>
+                    <p>This was the last chat line</p>
+                </div>
+                <div className="chatTrash">
+                    <DeleteIcon fontSize="large"/>
+                </div>
+            </div>
+
+        </div>
         </>
     )
 }
